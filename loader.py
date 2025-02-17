6246f54bc78b3c9244e9027dfce5ec316990dd61
1337 squad <cambridge1337@mail.ru>
2025-02-17 19:48:11 +0300

    loader.py

---
+++ loader.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+time.sleep(2)
+
+os.system('title Anti Expensive - Loader by 1337 SQUAD')
+
+print("""
+    _         _    _   ___                            _           
+   /_\   _ _ | |_ (_) | __|__ __ _ __  ___  _ _   ___(_)__ __ ___ 
+  / _ \ | ' \|  _|| | | _| \ \ /| '_ \/ -_)| ' \ (_-<| |\ V // -_)
+ /_/ \_\|_||_|\__||_| |___|/_\_\| .__/\___||_||_|/__/|_| \_/ \___|
+                                |_|                               
+""")
+
+a = input(" Запускать клиент? (Yes/No): ")
+
+if a == "Yes":
+    time.sleep(1)
+    print(" Запускаю client.jar")
+    time.sleep(1)
+    print(" Открываю нужные сайты")
+    time.sleep(1)
+    print(" Удачной игры браток! Лоудер закроется через 5 секунд")
+    os.system("C:/antiexpensive/client/1.16.5/client.jar")
+
+else:
+    print(" Абиднаю. Лоудер закроется через 5 секунд")
+
+time.sleep(5)
